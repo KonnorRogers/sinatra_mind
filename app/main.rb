9a57595881ab903e46058724c966fabdbaf79f13
paramagician <konnor5456@gmail.com>
2018-10-22 23:26:51 +0000

    troubleshooting

--- app/main.rb
+++ app/main.rb
@@ -7,25 +7,17 @@ require_relative 'sinatra_mind'
 
 enable :sessions
 
-game = SinatraMind::Game.new
-
 get '/' do
+  session[:game] ||= SinatraMind::Game.new
   redirect '/mastermind'
 end
 
 get '/mastermind' do
-  session[:game] ||= game
-  session[:array] ||= []
   @game = session[:game]
 
   @input = params['input']
   session[:array] << @input
   session[:game].take_turn(input: @input) unless @input.nil?
 
-  erb :mastermind, locals: {
-    game: @game,
-    input: @input,
-    array: session[:array],
-    guesses: @game.board.guess_array
-  }
+  erb :mastermind
 end
